f2f20109841f65360bf03e7b33aac43a9487d03f
Harish mahur <hmahur20@gmail.com>
2022-07-17 10:47:19 +0530

    welcome to snake and ladder game

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+package com.snake.ladder;
+
+public class SnakeAndLadder {
+	static int start_position = 0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to snake and ladder game");
+	}
+
+}
