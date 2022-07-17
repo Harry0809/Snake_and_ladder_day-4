9758b7d5fae28c3132517acae2383b85eb1f5a24
Harish mahur <hmahur20@gmail.com>
2022-07-17 11:13:10 +0530

    modified: player position in game

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,14 +1,39 @@
 package com.snake.ladder;
 
 public class SnakeAndLadder {
-	static int start_position = 0;
-
-	public static void main(String[] args) {
+	
+	
 		// TODO Auto-generated method stub
-		int dice = (int) (Math.random() * 10) % 6 + 1;
+		static final int initial_position = 0;
+		static final int no_play = 0;
+		static final int ladder = 1;
+		static final int snake = 2;
+		static int current_position = 0;
 
-		System.out.println("Welcome to Snake and ladder Game");
-		System.out.println("Number on dice:" + dice);
+		public static void main(String[] args) {
+			System.out.println("Welcome to Snake and ladder game");
+			while (current_position < 100) {
+				int dice = (int) (Math.random() * 10) % 6 + 1;
+				int option = (int) (Math.random() * 10) % 3;
+
+				if (option == ladder) {
+					System.out.println("ladder!!");
+					current_position += dice;
+
+				} else if (option == snake) {
+					System.out.println("snake!!");
+					current_position -= dice;
+
+				} else {
+					System.out.println("N0 play");
+
+				}
+				if (current_position < initial_position) {
+					current_position = initial_position;
+				}
+
+				System.out.println("currentposition: " + current_position);
+			}
+
+		}
 	}
-
-}
